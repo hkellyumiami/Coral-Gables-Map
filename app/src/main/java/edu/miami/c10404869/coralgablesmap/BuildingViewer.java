47dc2519790586c403959ebeed7f32560133dbc9
zrwmiller <zrwmiller@gmail.com>
2017-04-26 16:37:51 -0400

    Updated to include better documentation.

--- app/src/main/java/edu/miami/c10404869/coralgablesmap/BuildingViewer.java
+++ app/src/main/java/edu/miami/c10404869/coralgablesmap/BuildingViewer.java
@@ -20,9 +20,9 @@ import java.io.InputStream;
 public class BuildingViewer extends Activity {
 //-----------------------------------------------------------------------------
     private int view_id;
-    private String image_loc;
+    /*private String image_loc;
     private String audio_loc;
-    private String text_loc;
+    private String text_loc;*/
     private int next_view_id;
     private int prev_view_id;
     private int toggle_view_id;
@@ -34,12 +34,14 @@ public class BuildingViewer extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_building_viewer);
+
         //---Retrieve the appropriate view_id from the previous activity.
         Intent intent = getIntent();
         view_id = intent.getIntExtra("view_id",0);
-        //---Initialize the SQLite Database. (TO BE HADDED)
+
         //---Populate UI elements.
         populate();
+
         //---Set onSwipeTouchListener to handle swipes/clicks.
         ImageView main_image = (ImageView)findViewById(R.id.main_image);
         main_image.setOnTouchListener(new OnSwipeTouchListener(this) {
@@ -57,10 +59,12 @@ public class BuildingViewer extends Activity {
             }
             public void onClick() {
                 super.onClick();
+                //---Identify the UI elements.
                 TextView textView = (TextView)findViewById(R.id.text_desc);
                 Button extraImages = (Button)findViewById(R.id.extra_images);
                 Button toggleButton = (Button)findViewById(R.id.toggle);
                 LinearLayout audioControls = (LinearLayout)findViewById(R.id.audio_controls);
+                //---Change UI element visibility.
                 audioControls.setVisibility(View.VISIBLE);
                 textView.setVisibility(View.VISIBLE);
                 extraImages.setVisibility(View.GONE);
@@ -73,35 +77,48 @@ public class BuildingViewer extends Activity {
     @Override
     public void onPause() {
         super.onPause();
+        //---Pause audio if playing.
         if (mediaPlayer.isPlaying()) mediaPlayer.pause();
     }
 //-----------------------------------------------------------------------------
     @Override
     public void onDestroy() {
         super.onDestroy();
+        //---Stop audio and release player.
         if (mediaPlayer.isPlaying()) mediaPlayer.stop();
         if (mediaPlayer != null) mediaPlayer.release();
     }
 //-----------------------------------------------------------------------------
     public void populate() {
 
-        /*---In lieu of database functionality, uses switch statement to assign
-        the proper assets to the appropriate UI elements.*/
         //---Release mediaplayer if already exists.
         if (mediaPlayer != null) mediaPlayer.release();
         mediaPlayer = new MediaPlayer();
+
+        //---Identify UI elements to manipulate.
         ImageView mainImage = (ImageView)findViewById(R.id.main_image);
         TextView textView = (TextView)findViewById(R.id.text_desc);
         Button extraImages = (Button)findViewById(R.id.extra_images);
         Button toggleButton = (Button)findViewById(R.id.toggle);
         textView.setMovementMethod(new ScrollingMovementMethod());
+
+        //---Switch statement identified by the building's view_id.
         switch(view_id) {
             case 1:
+                //---Set the background.
                 mainImage.setImageResource(R.drawable.building01image01);
+
+                //---Set the text description.
                 textView.setText(R.string.building01text01);
+
+                //---Set the buttons.
                 extraImages.setBackgroundResource(R.drawable.building01externalbutton);
                 toggleButton.setBackgroundResource(R.drawable.building01internaltoggle);
+
+                //---Set the media player.
                 mediaPlayer = MediaPlayer.create(this,R.raw.building01audio01);
+
+                //---Set the variables for swipng, toggling, and extra ids.
                 next_view_id = 2;
                 prev_view_id = 3;
                 toggle_view_id = 4;
@@ -167,10 +184,14 @@ public class BuildingViewer extends Activity {
                 finish();
         }
 
-        /*---TO BE ADDED: The above switch statement will be replaced in order to
-        handle database queries to assign the same information. This will perform
-        a database query and populate UI elements (image, text, audio) based on
-        the info retrieved from the database query.*/
+        /*---The above switch statement should be replaced with an SQLite database
+        in order to handle database queries to assign the same information. Should
+        have fields for all of the global variables except mediaPlayer.
+
+        Queries will identify each field (strings should be the drawable location)
+        and then appropriately assign the variables. Instead of a switch statement,
+        the populate() method should query the database, assign the variables, and
+        update the views (buttons, textview, etc.) based on those variables.*/
     }
 //-----------------------------------------------------------------------------
     public void myClickHandler (View view) {
@@ -184,6 +205,7 @@ public class BuildingViewer extends Activity {
         //---Handle clicks.
         switch(view.getId()) {
             case R.id.extra_images:
+                //---Start the next activity based on the extra_id.
                 Intent data = new Intent();
                 data.setClassName("edu.miami.c10404869.coralgablesmap",
                         "edu.miami.c10404869.coralgablesmap.AdditionalImages");
@@ -191,21 +213,25 @@ public class BuildingViewer extends Activity {
                 startActivity(data);
                 break;
             case R.id.play:
+                //---Play the media player.
                 mediaPlayer.start();
                 break;
-            case R.id.toggle:
-                view_id = toggle_view_id;
-                populate();
             case R.id.pause:
+                //---Pause the audio.
                 mediaPlayer.pause();
                 break;
             case R.id.dismiss:
+                //---Stop the audio and change UI element visibility.
                 if (mediaPlayer.isPlaying()) mediaPlayer.stop();
                 audioControls.setVisibility(View.GONE);
                 textView.setVisibility(View.GONE);
                 extraImages.setVisibility(View.VISIBLE);
                 toggleButton.setVisibility(View.VISIBLE);
                 break;
+            case R.id.toggle:
+                //---Populate based on the toggle_view_id.
+                view_id = toggle_view_id;
+                populate();
             default:
                 break;
         }
