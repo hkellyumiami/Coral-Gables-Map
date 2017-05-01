579c0f7c626596f358ff9e0ceea0605512c89a85
zrwmiller <zrwmiller@gmail.com>
2017-05-01 11:36:04 -0400

    Updated menu. Added titles to additional images. Set text styles.

--- app/src/main/java/edu/miami/c10404869/coralgablesmap/AdditionalImages.java
+++ app/src/main/java/edu/miami/c10404869/coralgablesmap/AdditionalImages.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 //-----------------------------------------------------------------------------
 public class AdditionalImages extends Activity {
@@ -39,74 +40,89 @@ public class AdditionalImages extends Activity {
     public void populate() {
         //Populates the main image and indicates the next and previous images
         ImageView mainImage = (ImageView)findViewById(R.id.main_image);
+        TextView imageText = (TextView)findViewById(R.id.image_title);
         switch (extra_id) {
             case 1:
                 mainImage.setImageResource(R.drawable.building01exterior01);
+                imageText.setText(R.string.building01exterior01);
                 next_extra_id = 2;
                 prev_extra_id = 9;
                 break;
             case 2:
                 mainImage.setImageResource(R.drawable.building01exterior02);
+                imageText.setText(R.string.building01exterior02);
                 next_extra_id = 3;
                 prev_extra_id = 1;
                 break;
             case 3:
                 mainImage.setImageResource(R.drawable.building01exterior03);
+                imageText.setText(R.string.building01exterior03);
                 next_extra_id = 4;
                 prev_extra_id = 2;
                 break;
             case 4:
                 mainImage.setImageResource(R.drawable.building01exterior04);
+                imageText.setText(R.string.building01exterior04);
                 next_extra_id = 5;
                 prev_extra_id = 3;
                 break;
             case 5:
                 mainImage.setImageResource(R.drawable.building01exterior05);
+                imageText.setText(R.string.building01exterior05);
                 next_extra_id = 6;
                 prev_extra_id = 4;
                 break;
             case 6:
                 mainImage.setImageResource(R.drawable.building01exterior06);
+                imageText.setText(R.string.building01exterior06);
                 next_extra_id = 7;
                 prev_extra_id = 5;
                 break;
             case 7:
                 mainImage.setImageResource(R.drawable.building01exterior07);
+                imageText.setText(R.string.building01exterior07);
                 next_extra_id = 8;
                 prev_extra_id = 6;
                 break;
             case 8:
                 mainImage.setImageResource(R.drawable.building01exterior08);
+                imageText.setText(R.string.building01exterior08);
                 next_extra_id = 9;
                 prev_extra_id = 7;
                 break;
             case 9:
                 mainImage.setImageResource(R.drawable.building01exterior09);
+                imageText.setText(R.string.building01exterior09);
                 next_extra_id = 1;
                 prev_extra_id = 8;
                 break;
             case 10:
                 mainImage.setImageResource(R.drawable.building01interior01);
+                imageText.setText(R.string.building01interior01);
                 next_extra_id = 11;
                 prev_extra_id = 14;
                 break;
             case 11:
                 mainImage.setImageResource(R.drawable.building01interior02);
+                imageText.setText(R.string.building01interior02);
                 next_extra_id = 12;
                 prev_extra_id = 10;
                 break;
             case 12:
                 mainImage.setImageResource(R.drawable.building01interior03);
+                imageText.setText(R.string.building01interior03);
                 next_extra_id = 13;
                 prev_extra_id = 11;
                 break;
             case 13:
                 mainImage.setImageResource(R.drawable.building01interior04);
+                imageText.setText(R.string.building01interior04);
                 next_extra_id = 14;
                 prev_extra_id = 12;
                 break;
             case 14:
                 mainImage.setImageResource(R.drawable.building01interior05);
+                imageText.setText(R.string.building01interior05);
                 next_extra_id = 10;
                 prev_extra_id = 13;
                 break;
